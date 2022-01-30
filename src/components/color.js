132901d97f3145e5011a36324eda9314a6613c17
chrisumartinez <chrisumartinez@gmail.com>
2022-01-29 20:30:11 -0800

    Added more colors to color.js

--- src/components/color.js
+++ src/components/color.js
@@ -1,1 +1,5 @@
 export const bgColor = "#084C61";
+export const fontColor = "";
+export const secondaryBgColor = "#177E89";
+export const alarmColor = "#DB3A34";
+export const grayColor = "#323031";
